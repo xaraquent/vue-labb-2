e158207c85de0792ed6d3b8c42f0b5e72a22a2d6
xaraquent <simon.michael.te15d@gmail.com>
2025-03-03 11:42:22 +0100

    changed variable names

--- src/components/DataFetcher.vue
+++ src/components/DataFetcher.vue
@@ -1,34 +1,37 @@
 <template>
-    <div>
-        <h1>Data with Axios</h1>
-        <pre v-if="data">{{ data }}</pre>
-        <p v-else-if="error">Error: {{ error }}</p>
-        <p v-else>Loading...</p>
-    </div>
+  <div>
+    <h1>Data with Axios</h1>
+    <pre v-if="data">{{ data }}</pre>
+    <p v-else-if="error">Error: {{ error }}</p>
+    <p v-else>Loading...</p>
+  </div>
 </template>
 
 <script>
 import axios from 'axios';
 
 export default {
-    data() {
-        return {
-            data: null,
-            error: null,
-        };
-    },
-    async created() {
-        try {
-            const response = await axios.get('https://api.themoviedb.org/3/configuration', {
-                headers: {
-                    accept: 'application/json',
-                    Authorization: `Bearer ${import.meta.env.VITE_MOVIE_DB_API_KEY}`,
-                },
-            });
-            this.data = response.data;
-        } catch (err) {
-            this.error = err.message;
+  data() {
+    return {
+      data: null,
+      error: null,
+    };
+  },
+  async created() {
+    try {
+      const response = await axios.get(
+        'https://api.themoviedb.org/3/configuration',
+        {
+          headers: {
+            accept: 'application/json',
+            Authorization: `Bearer ${import.meta.env.MOVIE_DB_API_KEY}`,
+          },
         }
-    },
+      );
+      this.data = response.data;
+    } catch (err) {
+      this.error = err.message;
+    }
+  },
 };
 </script>
--- src/services/tmdb.js
+++ src/services/tmdb.js
@@ -1,17 +1,20 @@
 import axios from 'axios';
 
 export const getPopularMovies = async (page = 1) => {
-    try {
-        const response = await axios.get(`${import.meta.env.VITE_TMDB_BASE_URL}}/movie/popular`, {
-            params: {
-                api_key: import.meta.env.VITE_MOVIE_DB_API_KEY,
-                language: 'en-US',
-                page: page,
-            },
-        });
-        return response.data.results;
-    } catch (error) {
-        console.error('Failed to fetch movies:', error);
-        return [];
-    }
+  try {
+    const response = await axios.get(
+      `${import.meta.env.TMDB_BASE_URL}}/movie/popular`,
+      {
+        params: {
+          api_key: import.meta.env.MOVIE_DB_API_KEY,
+          language: 'en-US',
+          page: page,
+        },
+      }
+    );
+    return response.data.results;
+  } catch (error) {
+    console.error('Failed to fetch movies:', error);
+    return [];
+  }
 };
